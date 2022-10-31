1d2e10b465dc81950b6d22a3d345e2dabf27b8b7
garryfisher <gwinogradow@gmail.com>
2022-10-31 14:21:03 +0300

    FIX progression, gitignore

--- brain_games/games/game_progression.py
+++ brain_games/games/game_progression.py
@@ -2,18 +2,16 @@
 import random
 
 RULE = 'What number is missing in the progression?'
-START_STEP = 2
-END_STEP = 5
-START_CUT = 5
-END_CUT = 11
+START_STEP, END_STEP = 2, 5
+START_CUT, END_CUT = 5, 11
 SYMBOL = '..'
 
 
 
 def random_string(n1, n2=101):
     """Функция формирует строку нужной длины"""
-    step = random.randint(2, 5)
-    cut = random.randint(5, 11)
+    step = random.randint(START_STEP, END_STEP)
+    cut = random.randint(START_CUT, END_CUT)
     start = min(n1, n2)
     stop = max(n1, n2)
     return list(range(start, stop, step)[:cut])
