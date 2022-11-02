5fb8040fcbb6a5aef29e182bb1e67211153dd770
garryfisher <gwinogradow@gmail.com>
2022-11-02 12:46:06 +0300

    FIX remarks#2

--- brain_games/games/game_prime.py
+++ brain_games/games/game_prime.py
@@ -5,13 +5,10 @@ RULE = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 def calculation_game():
     num_1 = random.randint(1, 101)
-    question_generator = str(num1)
+    question_generator = str(num_1)
     index = 0
     for i in range(2, num_1 // 2 + 1):
         if (num_1 % i) == 0:
             index += 1
-    if index <= 0:
-        correct_answer = 'yes'
-    else:
-        correct_answer = 'no'
+    	    correct_answer = 'yes' if index <= 0 else 'no'
     return question_generator, correct_answer
