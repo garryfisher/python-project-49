8fc484d714e42d920260caa29d463305eedb9176
garryfisher <gwinogradow@gmail.com>
2022-11-09 12:48:47 +0300

    FIX games#2

--- brain_games/core.py
+++ brain_games/core.py
@@ -4,33 +4,22 @@ import prompt
 MAX_TRIES = 3
 
 
-def welcome_user(rule=""):
-    print('Welcome to the Brain Games!')
-    get_user_name = prompt.string('May I have your name? ')
-    print(f'Hello, {get_user_name}!')
-    print(f'{rule}')
-    return get_user_name
-
-
-def get_user_answer(question):
-    print(f"Question: {question}")
-    get_user_answer = input('Your answer: ')
-    return get_user_answer
-
-
 def run_game(game_name):
     number_of_tries = MAX_TRIES
-    name = welcome_user(game_name.RULE)
+    print('Welcome to the Brain Games!')
+    user_name = prompt.string('May I have your name? ')
+    print(f'Hello, {user_name}!')
+    print(f'{game_name.RULE}')
     while number_of_tries > 0:
         question, correct_answer = game_name.get_game()
-        user_answer = get_user_answer(question)
+        print(f"Question: {question}")
+        user_answer = input('Your answer: ')
         if user_answer == correct_answer:
             print('Correct!')
             number_of_tries -= 1
         else:
             print(f"'{user_answer}' is wrong answer ;(. "
                   f"Correct answer was '{correct_answer}'")
-            print(f"Let's try again, {name}!")
-            break
-    if number_of_tries == 0:
-        print(f"Congratulations, {name}!")
+            print(f"Let's try again, {user_name}!")
+            return
+    print(f"Congratulations, {user_name}!")
